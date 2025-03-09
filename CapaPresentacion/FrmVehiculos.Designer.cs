fea97436e13e72eb84238ab2157bc77fdfbb7fe4
marvingonzalez <mramirezg49@miumg.edu.gt>
2025-03-08 18:38:46 -0600

    Agregar archivos de proyecto.

---
+++ CapaPresentacion/FrmVehiculos.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmVehiculos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.gboxClientes = new System.Windows.Forms.GroupBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.cboxEstado = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFechaContratacion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVehiculoId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
+            this.gboxClientes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvEmpleados.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.GridColor = System.Drawing.Color.DarkGray;
+            this.dgvEmpleados.Location = new System.Drawing.Point(12, 334);
+            this.dgvEmpleados.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.ReadOnly = true;
+            this.dgvEmpleados.RowHeadersWidth = 51;
+            this.dgvEmpleados.RowTemplate.Height = 24;
+            this.dgvEmpleados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvEmpleados.Size = new System.Drawing.Size(1305, 410);
+            this.dgvEmpleados.TabIndex = 30;
+            this.dgvEmpleados.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpleados_CellClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnEliminar.Location = new System.Drawing.Point(975, 752);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(84, 69);
+            this.btnEliminar.TabIndex = 29;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // gboxClientes
+            // 
+            this.gboxClientes.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.gboxClientes.Controls.Add(this.txtApellido);
+            this.gboxClientes.Controls.Add(this.label9);
+            this.gboxClientes.Controls.Add(this.btnGuardar);
+            this.gboxClientes.Controls.Add(this.btnCancelar);
+            this.gboxClientes.Controls.Add(this.btnActualizar);
+            this.gboxClientes.Controls.Add(this.txtSalario);
+            this.gboxClientes.Controls.Add(this.cboxEstado);
+            this.gboxClientes.Controls.Add(this.label8);
+            this.gboxClientes.Controls.Add(this.label7);
+            this.gboxClientes.Controls.Add(this.txtFechaContratacion);
+            this.gboxClientes.Controls.Add(this.label4);
+            this.gboxClientes.Controls.Add(this.txtMarca);
+            this.gboxClientes.Controls.Add(this.label2);
+            this.gboxClientes.Controls.Add(this.txtVehiculoId);
+            this.gboxClientes.Controls.Add(this.label1);
+            this.gboxClientes.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gboxClientes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(34)))), ((int)(((byte)(118)))));
+            this.gboxClientes.Location = new System.Drawing.Point(12, 13);
+            this.gboxClientes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gboxClientes.Name = "gboxClientes";
+            this.gboxClientes.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gboxClientes.Size = new System.Drawing.Size(1305, 314);
+            this.gboxClientes.TabIndex = 28;
+            this.gboxClientes.TabStop = false;
+            this.gboxClientes.Text = "DATOS VEHICULOS";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Calisto MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(160, 159);
+            this.txtApellido.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(220, 32);
+            this.txtApellido.TabIndex = 27;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(8, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 25);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "Modelo:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(1092, 59);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(179, 69);
+            this.btnGuardar.TabIndex = 25;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(1092, 211);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(179, 84);
+            this.btnCancelar.TabIndex = 24;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(1092, 136);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(179, 69);
+            this.btnActualizar.TabIndex = 23;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Calisto MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(629, 96);
+            this.txtSalario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(220, 32);
+            this.txtSalario.TabIndex = 22;
+            // 
+            // cboxEstado
+            // 
+            this.cboxEstado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(34)))), ((int)(((byte)(118)))));
+            this.cboxEstado.FormattingEnabled = true;
+            this.cboxEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.cboxEstado.Location = new System.Drawing.Point(629, 148);
+            this.cboxEstado.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cboxEstado.Name = "cboxEstado";
+            this.cboxEstado.Size = new System.Drawing.Size(220, 36);
+            this.cboxEstado.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(459, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Estado:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(459, 96);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Salario:";
+            // 
+            // txtFechaContratacion
+            // 
+            this.txtFechaContratacion.Font = new System.Drawing.Font("Calisto MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaContratacion.Location = new System.Drawing.Point(220, 215);
+            this.txtFechaContratacion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtFechaContratacion.Name = "txtFechaContratacion";
+            this.txtFechaContratacion.Size = new System.Drawing.Size(220, 32);
+            this.txtFechaContratacion.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fecha Contratacion:";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Font = new System.Drawing.Font("Calisto MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMarca.Location = new System.Drawing.Point(160, 100);
+            this.txtMarca.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(232, 32);
+            this.txtMarca.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marca:";
+            // 
+            // txtVehiculoId
+            // 
+            this.txtVehiculoId.Font = new System.Drawing.Font("Calisto MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVehiculoId.Location = new System.Drawing.Point(160, 49);
+            this.txtVehiculoId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtVehiculoId.Name = "txtVehiculoId";
+            this.txtVehiculoId.Size = new System.Drawing.Size(220, 32);
+            this.txtVehiculoId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VehiculoID:";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnSalir.Location = new System.Drawing.Point(1089, 752);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(84, 69);
+            this.btnSalir.TabIndex = 31;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FrmVehiculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 845);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dgvEmpleados);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.gboxClientes);
+            this.Name = "FrmVehiculos";
+            this.Text = "FrmEmpleados";
+            this.Load += new System.EventHandler(this.FrmEmpleados_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
+            this.gboxClientes.ResumeLayout(false);
+            this.gboxClientes.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmpleados;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.GroupBox gboxClientes;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.ComboBox cboxEstado;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFechaContratacion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVehiculoId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ CapaPresentacion/FrmVehiculos.cs
@@ -0,0 +1,89 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FrmVehiculos : Form
+    {
+        public FrmVehiculos()
+        {
+            InitializeComponent();
+        }
+
+        public void MtdMostrarEmpleado()
+        {
+            CD_Empleados cd_empleado = new CD_Empleados();
+            DataTable dtMostrarEstudiantes = cd_empleado.MtdMostrarEmpleados();
+            dgvEmpleados.DataSource = dtMostrarEstudiantes;
+        }
+
+
+
+
+        private void FrmEmpleados_Load(object sender, EventArgs e)
+        {
+            MtdMostrarEmpleado();
+        }
+
+        private void dgvEmpleados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtVehiculoId.Text = dgvEmpleados.SelectedCells[0].Value.ToString();
+            txtMarca.Text = dgvEmpleados.SelectedCells[1].Value.ToString();
+            txtApellido.Text = dgvEmpleados.SelectedCells[2].Value.ToString();
+            txtFechaNacimiento.Text = dgvEmpleados.SelectedCells[3].Value.ToString();
+            txtFechaContratacion.Text = dgvEmpleados.SelectedCells[4].Value.ToString();
+            txtDepartamentoId.Text = dgvEmpleados.SelectedCells[5].Value.ToString();
+            txtPuestoTrabajo.Text = dgvEmpleados.SelectedCells[6].Value.ToString();
+            txtSalario.Text = dgvEmpleados.SelectedCells[7].Value.ToString();
+            cboxEstado.Text = dgvEmpleados.SelectedCells[8].Value.ToString();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtVehiculoId.Clear();
+            txtMarca.Clear();
+            txtApellido.Clear();
+            txtFechaNacimiento.Clear();
+            txtFechaContratacion.Clear();
+            txtDepartamentoId.Clear();
+            txtPuestoTrabajo.Clear();
+            txtSalario.Clear();
+            cboxEstado.Text="";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            CD_Empleados cd_empleado = new CD_Empleados();
+
+            try
+            {
+                cd_empleado.MtdAgregarEmpleados(txtMarca.Text, txtApellido.Text, txtFechaNacimiento.Text, txtFechaContratacion.Text, txtDepartamentoId.Text, txtPuestoTrabajo.Text, txtSalario.Text, cboxEstado.Text);
+                MessageBox.Show("El empleado se agrego con exito", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                MtdMostrarEmpleado();
+
+
+            }
+            catch (Exception ex)
+
+            {
+                MessageBox.Show(ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+            }
+        }
+    }
+}
